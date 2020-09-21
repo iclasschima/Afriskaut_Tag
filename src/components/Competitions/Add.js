5fa3f77a4e42e3400022311536523b5e094605d5
iClass <iclasschima@gmail.com>
2020-09-21 16:59:17 +0100

    create competition

---
+++ src/components/Competitions/Add.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "../../styles/create-team.scss";
+import { RiTrophyLine } from "react-icons/ri";
+import TextField from "@material-ui/core/TextField";
+
+export default function CreateTeam() {
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push("/add-players");
+  };
+
+  return (
+    <div className="create-team container-fluid mb-3">
+      <div className="row">
+        <div className="col-12">
+          <p className="header">
+            <RiTrophyLine /> Create Competition
+          </p>
+        </div>
+      </div>
+      <div className="row mt-4 justify-content-center">
+        <div className="col-lg-6 px-lg-4">
+          <form autoComplete="off">
+            <div className="form-group">
+              <label>Competition Name</label>
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                placeholder="Nigeria Professional Football League"
+              />
+            </div>
+            <div className="form-group">
+              <label>Abbreviation</label>
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                placeholder="NPFL"
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Country</label>
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                placeholder="Nigeria"
+              />
+            </div>
+            <div className="form-group">
+              <label>Season</label>
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                placeholder="2020/2021"
+              />
+            </div>
+            <div className="form-group">
+              <label>Type</label>
+              <select className="form-control">
+                <option>Select type</option>
+                <option>League</option>
+                <option>Cup</option>
+              </select>
+            </div>
+
+            <div className="form-group">
+              <label>Max Number of Teams</label>
+              <input
+                className="form-control"
+                type="number"
+                name="name"
+                placeholder="20"
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Competition Image</label>
+              <input
+                className="form-control"
+                type="file"
+                name="name"
+                placeholder="2020/2021"
+              />
+            </div>
+
+            <button
+              className="btn primary-btn form-control mt-2"
+              onClick={handleSubmit}
+            >
+              Create Competition
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Table from "./Table"
+import { useLocation } from "react-router-dom"
+import { Helmet } from "react-helmet";
 import { BiClipboard } from "react-icons/bi";
 import "../../styles/teams.scss"
 import teams from "../../helpers/teams"
@@ -7,13 +9,33 @@ import teams from "../../helpers/teams"
 export default function Teams() {
 
     return (
-        <div className="teams container-fluid">
-            <div className="row">
-                <div className="col-12 mb-3">
-                     <p className="header"> <BiClipboard />All Teams</p>
-                </div>
-            </div>
-            <Table teams={teams} />
+      <div className="teams container-fluid">
+        <Helmet>
+          <title>Afriskaut - Teams</title>
+        </Helmet>
+        {/* <nav aria-label="breadcrumb" className="col-7">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item active" aria-current="page">
+              Teams
+            </li>
+            <li class="breadcrumb-item">
+              <a href="/">Library</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+              Data
+            </li>
+          </ol>
+        </nav> */}
+        <div className="row">
+          <div className="col-12 mb-3">
+            <p className="header">
+              {" "}
+              <BiClipboard />
+              All Teams
+            </p>
+          </div>
         </div>
-    )
+        <Table teams={teams} />
+      </div>
+    );
 }
